09d7ecb5e4d3e2216d3c7a173e3ea161bb02c0ac
SOURISH SRIVASTAVA <sourish.srivastava.02@gmail.com>
2026-03-15 13:12:07 +0530

    Add linear regression closed form implementation
    Implement linear regression using closed-form solution with MSE and R2 score calculations.

---
+++ src/linear_regression_closed_form.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+
+data= pd.read_csv("train.csv")
+X_train = data['x'].values.reshape(-1,1)
+y_train = data['y'].values.reshape(-1,1)
+X_train = np.c_[np.ones(X_train.shape[0]), X_train] # Column concatenation of bias (a column of all 1)
+
+# theta = np.linalg.inv(X.T @ X) @ X.T @ y
+theta = np.linalg.pinv(X_train) @ y_train
+# We compute the Moore-Penrose Pseudo-inverse of X, X+= (((X^T)(X))^(-1))(X^T)
+
+check= pd.read_csv("test.csv")
+X_test = check['x'].values.reshape(-1,1)
+y_test = check['y'].values.reshape(-1,1)
+X_test = np.c_[np.ones(X_test.shape[0]), X_test]
+y_pred = X_test @ theta
+
+Mean_squared_error = np.mean((y_test - y_pred)**2)
+print("MSE:", Mean_squared_error)
+
+Residual_sum_of_squares = np.sum((y_test - y_pred)**2)
+Total_sum_of_squares = np.sum((y_test - np.mean(y_test))**2)
+r2 = 1 - (Residual_sum_of_squares/Total_sum_of_squares)
+print("R2 score:", r2)
+
+# MSE: 9.434852832251472
+# R2 score: 0.9887991524196075
